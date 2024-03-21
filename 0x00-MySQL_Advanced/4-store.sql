3488062ed6cbbd89a588767d25f7420c33df3477
siphoprince <mabirimisesipho@gmail.com>
2024-03-21 03:36:40 -0700

    store

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,8 @@
+-- Create trigger if it does not exist
+CREATE TRIGGER decrease_quantity AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    UPDATE items
+    SET quantity = quantity - NEW.number
+    WHERE name = NEW.item_name;
+END;
