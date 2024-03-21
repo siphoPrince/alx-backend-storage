4c94073fd132692c52f74bd5f7fce8ffc2526546
siphoprince <mabirimisesipho@gmail.com>
2024-03-21 03:54:21 -0700

    create meeting

---
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,6 @@
+-- Create view if it does not exist
+
+CREATE OR REPLACE VIEW need_meeting AS
+SELECT name
+FROM students
+WHERE (score < 80 OR last_meeting IS NULL OR last_meeting < DATE_SUB(NOW(), INTERVAL 1 MONTH));
