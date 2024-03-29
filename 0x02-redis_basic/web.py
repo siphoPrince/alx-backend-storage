46bde85336c298bba590aca9147c906c21eaec97
siphoprince <mabirimisesipho@gmail.com>
2024-03-29 08:57:31 -0700

    pycode

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -8,12 +8,13 @@ import functools
 
 
 def track_url_access_count(method):
+    """track url"""
     @functools.wraps(method)
     def wrapper(url, *args, **kwargs):
-        # Initialize Redis client
+        """Initialize Redis client"""
         redis_client = redis.Redis()
 
-        # Track URL access count
+        """ Track URL access count"""
         count_key = f"count:{url}"
         access_count = redis_client.incr(count_key)
 
@@ -21,21 +22,22 @@ def track_url_access_count(method):
     return wrapper
 
 def cache_content(method):
+    """main cahce"""
     @functools.wraps(method)
     def wrapper(url, *args, **kwargs):
-        # Initialize Redis client
+        """ Initialize Redis client"""
         redis_client = redis.Redis()
 
-        # If URL content is cached, return it
+        """ If URL content is cached, return it"""
         cache_key = f"content:{url}"
         cached_content = redis_client.get(cache_key)
         if cached_content:
             return cached_content.decode("utf-8")
 
-        # Retrieve HTML content from URL
+        """ Retrieve HTML content from URL"""
         content = method(url, *args, **kwargs)
 
-        # Cache HTML content with expiration time of 10 seconds
+        """Cache HTML content with expiration time of 10 seconds"""
         redis_client.setex(cache_key, 10, content)
 
         return content
@@ -44,6 +46,7 @@ def cache_content(method):
 @track_url_access_count
 @cache_content
 def get_page(url: str) -> str:
+    """get page"""
     response = requests.get(url)
     return response.text
 
